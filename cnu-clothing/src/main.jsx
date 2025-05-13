d0e22686862f0314a132749bb28b0fb795fba0ba
Superkid0714 <kid1412wer@gmail.com>
2025-05-14 00:51:32 +0900

    오타수정

--- cnu-clothing/src/main.jsx
+++ cnu-clothing/src/main.jsx
@@ -9,7 +9,7 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider clinet={queryClient}>
+    <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
   </StrictMode>
