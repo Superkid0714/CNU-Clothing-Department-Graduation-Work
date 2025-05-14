9944f73834737a90e08e7fb91d8b176453ea4650
Superkid0714 <kid1412wer@gmail.com>
2025-05-14 21:59:56 +0900

    업데이트

--- cnu-clothing/src/components/PreviewPosterPage.jsx
+++ cnu-clothing/src/components/PreviewPosterPage.jsx
@@ -25,7 +25,7 @@ export default function PreviewPosterPage() {
         <div className="mt-4 space-y-1 text-sm md:text-base text-gray-300">
           <p>06.06. ~ 06.11.</p>
           <p className="font-semibold text-white">
-            전남대학교 의류학과 2024년도 졸업작품 전시회
+            전남대학교 의류학과 2025년도 졸업작품 전시회
           </p>
           <p>전남대학교 광주캠퍼스 스토리움</p>
         </div>
@@ -33,7 +33,7 @@ export default function PreviewPosterPage() {
 
       {/* 버튼 */}
       <button
-        onClick={() => alert('전시 시작하기 버튼 클릭됨')}
+        onClick={() => navigate('/main')}
         className="mt-12 px-6 py-2 rounded-full bg-white text-black text-sm md:text-base font-semibold hover:bg-gray-100 transition"
       >
         전시 시작하기
